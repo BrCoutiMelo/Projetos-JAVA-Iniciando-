184528d086bea75de64b4d6510e46228ec92a885
Breno <brenocoutinhoicm@hotmail.com>
2020-09-10 17:45:33 -0300

    EXERCICIO SOBRE NUMEROS PARES CONCLUÍDO

--- ola mundo/src/exercicios.java
+++ ola mundo/src/exercicios.java
@@ -2,16 +2,20 @@ import java.util.Scanner;
 import java.util.Locale;
 class exercicios {
 	public static void main(String[]args) {
-		Scanner vc = new Scanner(System.in); //Imprimir numeros pares de 25;
+		Scanner n = new Scanner(System.in); //Imprimir numeros pares de 25;
 		int contador = 0;
-		while(contador<=5) {
-			int numeros = vc.nextInt();
-			if(numeros % 2 ==0)
-				contador =+ 1;
+		int andador = 0;
+		while(andador<=5) {
+			int numeros = n.nextInt();
+			if(numeros%2==0)
+				contador+=1;
+				andador+=1;
+			if(numeros%2!=0)
+				andador += 1;
 			
-			System.out.println("Numeros pares dentre os 25 numeros: "+contador);
 		}
 		
+		System.out.println("a quantidade de numeros pares é: "+contador);
 		
 	}
 	
